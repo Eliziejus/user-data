3e3ce72af4ed1257001d583dc68a52a632f5b711
Eliziejus <e.capas@salvum.lt>
2022-01-28 12:18:16 +0200

    Fixing Edit Page

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {DataService} from "../services/data.service";
+import {Profile} from "../models/profile.model";
 
 @Component({
   selector: 'app-edit',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private dataService: DataService,
+  ) { }
 
   ngOnInit(): void {
+
+    this.route.paramMap.subscribe(params => {
+      const profileId = params.get('id') as null;
+      if(profileId) {
+        this.getProfileData(profileId);
+      }
+    });
+  }
+
+  getProfileData(id: number) {
+    this.dataService.getProfile(id).subscribe(
+      (profile: Profile) => this.editProfile(profile),
+    );
+  }
+
+  editProfile(profile: Profile){
+    this.profileForm.patchValue({
+      name: profile.name,
+      surname: profile.surname,
+      birthday: profile.birthday,
+      gender: profile.gender,
+      phoneNumber: profile.phoneNumber,
+      personalId: profile.personalId,
+    });
   }
 
 }
--- src/app/table-data/table-data.component.ts
+++ src/app/table-data/table-data.component.ts
@@ -1,17 +1,16 @@
 import {ChangeDetectorRef, Component, Input, OnChanges} from '@angular/core';
 import {Profile} from "../models/profile.model";
-import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-table-data',
   templateUrl: './table-data.component.html',
   styleUrls: ['./table-data.component.scss'],
 })
-export class TableDataComponent implements OnChanges{
+export class TableDataComponent {
 
   @Input() profiles: Profile[];
 
-  constructor(private cdr: ChangeDetectorRef, private route: ActivatedRoute, private router: Router){
+  constructor(private cdr: ChangeDetectorRef, private router: Router){
   }
 
   public getDate(birthday: string) {
@@ -28,10 +27,6 @@ export class TableDataComponent implements OnChanges{
     }
   }
 
-  public ngOnChanges() {
-    this.cdr.detectChanges();
-
-  }
 
   editProfileData(id: number){
     this.router.navigate(['/edit', id])
